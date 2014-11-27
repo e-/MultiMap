a943ec42215534379becdab7854ca624c4add2a2
Jaemin Jo <jmjo@hcil.snu.ac.kr>
2014-11-27 17:23:32 +0900

    add nmap

--- js/app.js
+++ js/app.js
@@ -14,12 +14,12 @@ requirejs.config({
   }
 });
 
-requirejs(['jquery', 'd3'
+requirejs(['jquery', 'd3', 'nmap'
 ], function($
 ){
   
   d3.json('data/data.json', function(data){
-    var svg = d3.select('body').append('svg').attr('width', 1000).attr('height', 1000);
+    var svg = d3.select('body').append('svg').attr('width', 1900).attr('height', 1000);
     var leaves = [];
    
     function draw(d){
@@ -34,12 +34,61 @@ requirejs(['jquery', 'd3'
 
     var domain = d3.extent(leaves, function(leaf){return leaf.popRatio;}),
         color = d3.scale.linear().domain([domain[0], 100, domain[1]]).range(['#67a9cf', '#f7f7f7', '#ef8a62']);
-
+  
     leaves.forEach(function(leaf){
+      leaf.areas = [];
+
       leaf.d.forEach(function(d){
-        svg.append('path').attr('d', d).attr('fill', color(leaf.popRatio)).attr('stroke', '#aaa').attr('stroke-width', 1);
+        var path = svg.append('path').attr('d', d).attr('fill', color(leaf.popRatio)).attr('stroke', '#aaa').attr('stroke-width', 1);
+        
+        leaf.areas.push(path);
       });
     });
 
+    var nmap = d3.layout.nmap().value(function(node){return node.population;}).width(500).height(500);
+    var g = svg.append('g');
+
+    var pattern = /M([\d|\.]*),([\d|\.]*)/,
+        pattern2 = /l([-|\d|\.]*),([-|\d|\.]*)/g
+    ;
+
+    leaves.forEach(function(leaf){
+      var element = leaf.areas[0].node(),
+          bbox = element.getBBox();
+
+      leaf.ix = bbox.x + bbox.width / 2;
+      leaf.iy = bbox.y + bbox.height / 2;
+    });
+    
+    function translate(x, y){
+      return 'translate(' + x + ',' + y + ')';
+    }
+    
+    g.attr('transform', translate(600, 100));
+    nmap(leaves);
+    g
+      .selectAll('rect')
+      .data(leaves)
+      .enter()
+      .append('rect')
+        .attr('width', function(leaf){return leaf.dx;})
+        .attr('height', function(leaf){return leaf.dy;})
+        .attr('fill', function(leaf){return color(leaf.popRatio)})
+        .attr('transform', function(leaf){return translate(leaf.x, leaf.y);})
+        .attr('stroke', '#aaa')
+        .attr('stroke-width', 1)
+        .on('mouseover', function(leaf){
+          leaf.areas.forEach(function(path){
+            path.attr('fill', '#666');
+          });
+        })
+        .on('mouseout', function(leaf){
+          leaf.areas.forEach(function(path){
+            path.attr('fill', color(leaf.popRatio));
+          });
+        });
+
+
+    ;
   });
 });
---
+++ js/nmap.js
@@ -0,0 +1,183 @@
+define(['d3'], function(){
+  d3.layout.nmap = function(){
+    var HORIZONTAL = 1, VERTICAL = 2,
+        ix = function(node){return node.ix;},
+        iy = function(node){return node.iy;},
+        value = function(node){return node.value},
+        width = 1,
+        height = 1;
+
+    function inverse(a){
+      if(a>1)return 1/a;
+      return a;
+    }
+
+    function split(nodes, x1, y1, x2, y2, direction){
+      if(nodes.length == 0)
+        return;
+
+      if(nodes.length == 1) {
+        var node = nodes[0];
+        node.x = x1;
+        node.y = y1;
+        node.dx = x2 - x1;
+        node.dy = y2 - y1;
+        return;
+      }
+
+      nodes = nodes.slice(0);
+      var length = nodes.length;
+      /*if(direction == HORIZONTAL) {
+        nodes.sort(function(a, b){
+          return a._ix - b._ix;
+        });
+      } else {
+        nodes.sort(function(a, b){
+          return a._iy - b._iy;
+        });
+      }
+      
+      var pivot = Math.floor(length / 2),
+          left = pivot, // 0 ~ pivot - 1
+          right = length - pivot,
+          sumLeft = 0,
+          sumRight = 0,
+          i; // pivot ~ length - 1
+      
+      for(i = 0; i < left; ++i) {
+        sumLeft += value(nodes[i]);
+      }
+
+      for(i = pivot; i < length; ++i) {
+        sumRight += value(nodes[i]);
+      }
+
+      var ratioLeft = sumLeft / (sumLeft + sumRight),
+          nodesLeft = nodes.filter(function(_, i){return i < pivot;}),
+          nodesRight = nodes.filter(function(_, i){return i >= pivot;})
+      ;
+      
+      if(direction == HORIZONTAL) {
+        split(nodesLeft, x1, y1, x1 + (x2 - x1) * ratioLeft, y2, VERTICAL);
+        split(nodesRight, x1 + (x2 - x1) * ratioLeft, y1, x2, y2, VERTICAL);
+      } else {
+        split(nodesLeft, x1, y1, x2, y1 + (y2 - y1) * ratioLeft, HORIZONTAL);
+        split(nodesRight, x1, y1 + (y2 - y1) * ratioLeft, x2, y2, HORIZONTAL);
+      }*/
+      
+      var pivot = Math.floor(length / 2),
+          left = pivot, // 0 ~ pivot - 1
+          right = length - pivot,
+          sumLeft = 0,
+          sumRight = 0,
+          i; // pivot ~ length - 1
+      
+      sumLeft = 0;
+      sumRight = 0;
+
+      nodes.sort(function(a, b){
+        return a._ix - b._ix;
+      });
+      
+      for(i = 0; i < left; ++i) {
+        sumLeft += value(nodes[i]);
+      }
+
+      for(i = pivot; i < length; ++i) {
+        sumRight += value(nodes[i]);
+      }
+
+      var ratioLeft = sumLeft / (sumLeft + sumRight),
+          nodesLeft = nodes.filter(function(_, i){return i < pivot;}),
+          nodesRight = nodes.filter(function(_, i){return i >= pivot;})
+      ;
+      
+      var verWidth1 = (x2 - x1) * ratioLeft,
+          verWidth2 = (x2 - x1) * (1 - ratioLeft),
+          verHeight = y2 - y1;
+
+      nodes.sort(function(a, b){
+        return a._iy - b._iy;
+      });
+      
+      sumLeft = 0;
+      sumRight = 0;
+
+      for(i = 0; i < left; ++i) {
+        sumLeft += value(nodes[i]);
+      }
+
+      for(i = pivot; i < length; ++i) {
+        sumRight += value(nodes[i]);
+      }
+
+      var ratioTop = sumLeft / (sumLeft + sumRight),
+          nodesTop = nodes.filter(function(_, i){return i < pivot;}),
+          nodesBottom = nodes.filter(function(_, i){return i >= pivot;}),
+          horWidth = x2 - x1;
+          horHeight1 = (y2 - y1) * ratioTop,
+          horHeight2 = (y2 - y1) * (1 - ratioTop)
+
+      if(inverse(verWidth1 / verHeight) * inverse(verWidth2 / verHeight) > inverse(horWidth / horHeight1) * inverse(horWidth / horHeight2)) {
+        split(nodesLeft, x1, y1, x1 + (x2 - x1) * ratioLeft, y2, VERTICAL);
+        split(nodesRight, x1 + (x2 - x1) * ratioLeft, y1, x2, y2, VERTICAL);
+      } else {
+        split(nodesTop, x1, y1, x2, y1 + (y2 - y1) * ratioLeft, HORIZONTAL);
+        split(nodesBottom, x1, y1 + (y2 - y1) * ratioLeft, x2, y2, HORIZONTAL);
+      }
+    }
+
+    function nmap(nodes){
+      nodes.forEach(function(node){
+        node._ix = ix(node);
+        node._iy = iy(node);
+      });
+      
+      var xExtent = d3.extent(nodes, function(node){return node._ix;}),
+          yExtent = d3.extent(nodes, function(node){return node._iy;})
+      ;
+      
+      // normalized
+      nodes.forEach(function(node){
+        node._ix = (node._ix - xExtent[0]) / xExtent[1];
+        node._iy = (node._iy - yExtent[0]) / yExtent[1];
+      });
+
+      split(nodes, 0, 0, 1, 1, HORIZONTAL);
+      
+      nodes.forEach(function(node){
+        node.x *= width;
+        node.y *= height;
+        node.dx *= width;
+        node.dy *= height;
+      });
+    }
+
+    nmap.value = function(value2){
+      value = value2;
+      return nmap;
+    };
+
+    nmap.ix = function(value){
+      ix = value;
+      return nmap;
+    };
+
+    nmap.iy = function(value){
+      iy = value;
+      return nmap;
+    };
+
+    nmap.width = function(value){
+      width = value;
+      return nmap;
+    };
+
+    nmap.height = function(value) {
+      height = value;
+      return nmap;
+    };
+
+    return nmap;
+  };
+});
